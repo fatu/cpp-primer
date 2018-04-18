4c8cf63d1f29a5185ed23f632e85296646e3762d
fatu <fatu@paypal.com>
2018-04-17 18:28:23 -0700

    Added rw_sales_items.

---
+++ source/ch1/rw_sales_items.cpp
@@ -0,0 +1,15 @@
+//
+// Created by Tu, Fangbo on 4/17/18.
+//
+#include <iostream>
+#include "Sales_item.h"
+
+int main()
+{
+  Sales_item book;
+
+  std::cin >> book;
+
+  std::cout << book << std::endl;
+  return 0;
+}
