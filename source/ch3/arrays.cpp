47aac81c4af644d077103489611892f4565617ba
fatu <fatu@paypal.com>
2018-05-11 21:31:14 -0700

    ch3 finished.

--- source/ch3/arrays.cpp
+++ source/ch3/arrays.cpp
@@ -4,20 +4,18 @@
 #include <iostream>
 #include <string>
 #include <iterator>
+#include <vector>
 using std::begin;
 using std::end;
 using std::string;
 using std::cin;
 using std::cout;
 using std::endl;
+using std::vector;
 
-string sa[10];
-int ia[10];
 
 int main()
 {
-    cout << *sa << endl;
-    cout << *ia << endl;
     string sa2[10];
     int    ia2[10];
     cout << *sa2 << endl;
@@ -63,4 +61,64 @@ int main()
 
     char* ca = "haha";
     cout << strlen(ca) << endl;
+
+    // Array to initialize a vector
+    int int_arr[] = {0, 1, 2, 3, 4, 5};
+    vector<int> ivec(begin(int_arr), end(int_arr));
+    vector<int> subVec(int_arr + 1, int_arr + 4);
+
+    int ia[3][4];
+    int arr1[10][20][30] = {0};
+
+    ia[2][3] = arr1[0][0][0];
+    int (&row)[4] = ia[1];
+
+    constexpr size_t rowCnt = 3, colCnt = 4;
+    int ia1[rowCnt][colCnt];
+    // for each row
+    for (size_t i = 0; i != rowCnt; ++i) {
+        // for each column within the row
+        for (size_t j = 0; j != colCnt; ++j) {
+            // assign the element's positional index as its vale
+            ia[i][j] = i * colCnt + j;
+        }
+    }
+
+    size_t cnt1 = 0;
+    for (auto &row : ia) {
+        for (auto &col : row) {
+            col = cnt;
+            ++cnt;
+        }
+    }
+
+    for (const auto &row : ia) {
+        for (auto col : row) {
+            cout << col << endl;
+        }
+    }
+
+    cout << "ia = " << ia[0][0] << endl;
+
+    for (auto p = ia; p != ia + 3; ++p) {
+        for (auto q = *p; q != *p + 4; ++q)
+            cout << *q << ' ';
+        cout << endl;
+    }
+
+    for (auto p = begin(ia); p != end(ia); ++p) {
+        for (auto q = begin(*p); q != end(*p); ++q)
+            cout << *q << ' ';
+        cout << endl;
+    }
+
+    using int_array = int[4];
+    typedef int int_array[4];
+
+    for (int_array *p = ia; p != ia + 3; ++p) {
+        for (int *q = *p; q != *p + 4; ++q) {
+            cout << *q << ' ';
+        }
+        cout << endl;
+    }
 }
