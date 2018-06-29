14262db7d24623e9b7c3277b9308aee973df9b37
fatu <fatu@paypal.com>
2018-06-29 09:54:06 -0700

    Added unreal project.

---
+++ unreal/Section_02/BullCowGame/BullCowGame/main.cpp
@@ -0,0 +1,20 @@
+//
+//  main.cpp
+//  BullCowGame
+//
+//  Created by Tu, Fangbo on 6/24/18.
+//  Copyright © 2018 Tu, Fangbo. All rights reserved.
+//
+
+
+#include <iostream>
+
+int doubleMe(int number) {
+    return number*2;
+}
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n" << doubleMe(4) << std::endl;
+    return 0;
+}
